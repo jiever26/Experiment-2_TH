0529fdf2e837d44b709a74d814549dc24cebb8bd
jiever26 <47727484+jiever26@users.noreply.github.com>
2019-05-05 20:05:21 +0800

    Add files via upload

---
+++ Problem_3.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <math.h>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double var1,var,v,y;
+	const double z=2.5;
+	int x;
+	
+	cout <<"Place your desired value of x: ";
+	cin>> x;
+	cout << "\nPlace your desired value of y: ";
+	cin>>y;
+
+switch(x)
+{
+	case 1: 
+      if(y>1&&y<5){
+      v=x*y*z;
+	  cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+else if (y>=5)
+{
+  	v=x+(y/z);
+    cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+else
+{
+    v=x+y+z;
+    cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+break;
+  
+    case 2:
+      if(y<=5)
+{
+	var1=(x-y)/z;
+    v=abs(var1);
+    cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+else if (y>5)
+{
+    var=sqrt(y+z);
+    v=x-var;
+    cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+else
+{
+    v=x+y+z;
+    cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+break;
+
+    default:
+      v=x+y+z;
+      cout << "\nV is: "<<setprecision(2)<<setw(10)<<v;
+}
+
+  getch();
+  return 0;
+
+}
+
