2ceffacdf16cd73ceb66ce55a6e23e68001982f3
jiever26 <47727484+jiever26@users.noreply.github.com>
2019-05-05 14:51:20 +0800

    Add files via upload

---
+++ Problem_4.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+int main () 
+{
+	int x=0;
+	cout<<"Counting...\n\n"<<endl;
+
+while(x<31)
+{
+	cout << x << ",";
+	x++;
+
+    if (x==11)
+	{
+		x++;continue;
+	}
+	else if(x==13)
+	{
+		x++;continue;
+	}
+	else if(x==15)
+	{
+		x++;continue;
+	}
+	else if(x==17)
+	{
+		x++;continue;
+	}
+	else if(x==19)
+	{
+		x++;continue;
+	}
+	else if(x==21)
+	{
+		x++;continue;
+	}
+	else if(x==23)
+	{
+		x++;continue;
+	}
+	else if(x==25)
+	{
+		x++;continue;
+	}
+	else if(x==27)
+	{
+		x++;continue;
+	}
+	else if(x==29)
+	{
+		x++;continue;
+	}
+}
+  getch();
+  return 0;
+}
